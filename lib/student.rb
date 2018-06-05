67a03fcb15d8c42807d8a53fbc6336f67516ea35
Isaac Villicana <villicana.i@gmail.com>
2018-06-05 13:22:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,4 +96,13 @@ class Student
         SQL
     DB[:conn].execute(sql).map { |row| self.new_from_db(row)}.first
   end
+
+  def self.all_students_in_grade_X (x)
+    sql = <<-SQL
+      SELECT *
+        FROM students
+        WHERE grade = ?
+        SQL
+    DB[:conn].execute(sql, x)
+  end
 end
